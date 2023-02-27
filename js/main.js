cb4d92acd5b236981d5950546bf756b0781a6e27
dewykit <87225225+dewykit@users.noreply.github.com>
2023-02-27 18:39:30 +1100

    Simplified landing page

--- js/main.js
+++ js/main.js
@@ -1,85 +1,14 @@
-const track = document.getElementById("image-track");
+/* -- Glow effect -- */
 
-const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+const blobs = document.querySelectorAll("#blob");
 
-const handleOnUp = () => {
-  track.dataset.mouseDownAt = "0";  
-  track.dataset.prevPercentage = track.dataset.percentage;
-}
-
-const handleOnMove = e => {
-  if(track.dataset.mouseDownAt === "0") return;
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
   
-  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-        maxDelta = window.innerWidth / 2;
-  
-  const percentage = (mouseDelta / maxDelta) * -100,
-        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-  
-  track.dataset.percentage = nextPercentage;
-  
-  track.animate({
-    transform: `translate(${nextPercentage}%, -50%)`
-  }, { duration: 1200, fill: "forwards" });
-  
-  for(const image of track.getElementsByClassName("image")) {
-    image.animate({
-      objectPosition: `${100 + nextPercentage}% center`
-    }, { duration: 1200, fill: "forwards" });
-  }
-}
-
-/* -- Had to add extra lines for touch events -- */
-
-window.onmousedown = e => handleOnDown(e);
-
-window.ontouchstart = e => handleOnDown(e.touches[0]);
-
-window.onmouseup = e => handleOnUp(e);
-
-window.ontouchend = e => handleOnUp(e.touches[0]);
-
-window.onmousemove = e => handleOnMove(e);
-
-window.ontouchmove = e => handleOnMove(e.touches[0]);
-
-// brand
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-function setIntervalForElement(element) {
-  let iteration = 0;
-
-  let interval = setInterval(() => {
-    element.innerText = element.innerText
-      .split("")
-      .map((letter, index) => {
-        if (index < iteration) {
-          return element.dataset.value[index];
-        }
-
-        return letters[Math.floor(Math.random() * 26)];
-      })
-      .join("");
-
-    if (iteration >= element.dataset.value.length) {
-      clearInterval(interval);
-    }
-
-    iteration += 1 / 3;
-  }, 30);
-
-  return interval;
-}
-
-const elements = document.querySelectorAll("h1");
-
-for (let i = 0; i < elements.length; i++) {
-  let element = elements[i];
-  let interval = null;
-
-  element.onmouseover = event => {
-    clearInterval(interval);
-    interval = setIntervalForElement(event.target);
-  };
+  blobs.forEach(blob => {
+    blob.animate({
+      left: `${clientX}px`,
+      top: `${clientY}px`
+    }, { duration: 3000, fill: "forwards" });
+  });
 }
